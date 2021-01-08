98b42e0d10912c74be4bca9a22a44a42e8f54d79
alerodriguezz <alerodriguez@csumb.edu>
2021-01-07 21:09:04 -0800

    fixed authentification issues

--- send_emails.py
+++ send_emails.py
@@ -1,14 +1,16 @@
+#!/usr/bin/env python3
+
 import smtplib
 import os 
 
 #which email is this being sent from?
-sender_email = os.environ.get('SENDER_EMAIL')
+sender_email = str(os.environ.get('SENDER_EMAIL'))
 
 #pass for sender's account 
-password = os.environ.get('PASSWORD')
+password = str(os.environ.get('PASSWORD'))
 
 #who is this mail going to be sent to 
-recipient_email='archer.al@mail.com'
+recipient_email='alex4dodgers@yahoo.com'
 
 #email context
 
@@ -18,15 +20,15 @@ Hello there.
 def send_email():
     
     #setup email server, email host , and common used port 
-    server = smtplib.SMTP('smtp.mail.com',587)
+    server = smtplib.SMTP('smtp.gmail.com',587)
     
     #encrypts smtp commands
     server.starttls()
-    
+
     #Login to senders' email account 
     server.login(sender_email, password)
 
-    #
+    #Send the email
     server.sendmail(sender_email, recipient_email, email_text)
 
     server.quit()
