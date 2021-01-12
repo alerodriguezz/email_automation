f01c0f1e0b90a7cb365db5c0fcff928a8c252eb9
alerodriguezz <alerodriguez@csumb.edu>
2021-01-11 20:16:13 -0800

    added MIMEText and email.mime.text libraries

--- send_emails.py
+++ send_emails.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python3
 
 import smtplib
-import os 
+import os
+import email.utils
+from email.mime.text import MIMEText
 
 #which email is this being sent from?
 sender_email = str(os.environ.get('SENDER_EMAIL'))
+sender_name = "John"
 
 #pass for sender's account 
 password = str(os.environ.get('PASSWORD'))
 
 #who is this mail going to be sent to 
 recipient_email='alex4dodgers@yahoo.com'
+recipient_name= "Sterling"
 
 #email context
 
 email_text='''
-Hello there. 
+Hello there. Again.
 '''
 def send_email():
+    print("Sending email...\n")
+
+    message = MIMEText(email_text)
     
+    #Populate message object with data
+    message['To'] = email.utils.formataddr((recipient_name,recipient_email))
+    message['From'] = email.utils.formataddr((sender_name,sender_email))
+    message['Subject'] = "Test email"
     #setup email server, email host , and common used port 
     server = smtplib.SMTP('smtp.gmail.com',587)
     
@@ -29,9 +40,9 @@ def send_email():
     server.login(sender_email, password)
 
     #Send the email
-    server.sendmail(sender_email, recipient_email, email_text)
+    server.sendmail(sender_email, recipient_email, message.as_string())
 
     server.quit()
     pass
-
+    print("Email Sent. \n")
 send_email()
